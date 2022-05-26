898c8cbd6a63edc7e435ab77464b7bb6b9fe05c4
unknown <explotacionesalfa@gmail.com>
2022-05-26 01:30:33 -0300

    deploy

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig: {
+    apiKey: "AIzaSyBD5cyQyMkGloHsuRJ2KlyVy1Ss0nEP3VE",
+    authDomain: "portfolio-9d1e3.firebaseapp.com",
+    projectId: "portfolio-9d1e3",
+    storageBucket: "portfolio-9d1e3.appspot.com",
+    messagingSenderId: "260169390315",
+    appId: "1:260169390315:web:3eee47ce58b86005ce86ae",
+    measurementId: "G-P0RRN3DJ3V"
+  }
 };
